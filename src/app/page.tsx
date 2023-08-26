f76a7bb06febbcda8aedf7e3d51c8abb980114ce
Puwanart Y <puwanart.tin@gmail.com>
2023-08-26 13:22:10 +0700

    fix: :art: move homepage to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 import '../i18n'
+
 import Booking from '@/components/partials/Booking'
+import Homepage from '@/components/partials/Homepage'
 import Inbox from '@/components/partials/Inbox'
+import MyBooking from '@/components/partials/MyBooking'
 import Sorry from '@/components/partials/Sorry'
 import { LayoutContext } from '@/contexts/layout'
-import { FaMap } from '@react-icons/all-files/fa/FaMap'
-import { FaTicketAlt } from '@react-icons/all-files/fa/FaTicketAlt'
 import { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 
@@ -18,31 +19,13 @@ const App = () => {
 
   switch (currentState) {
     case 'homepage':
-      return (
-        <div className="flex gap-8">
-          {['map', 'booking'].map((item, index) => (
-            <div
-              key={index}
-              className="flex h-64 w-64 cursor-pointer flex-col items-center justify-center rounded-full bg-white shadow-2xl transition-all duration-200 hover:scale-110 "
-              onClick={() => {
-                onChangeState(item)
-              }}>
-              {item === 'map' ? (
-                <FaMap className="h-24 w-24 text-blue-800" />
-              ) : (
-                <FaTicketAlt className="h-24 w-24 text-blue-800" />
-              )}
-              <p className="mt-4 text-center text-2xl text-gray-500">{t(`homepage.${item}`)}</p>
-            </div>
-          ))}
-        </div>
-      )
+      return <Homepage />
     case 'map':
       return <Sorry />
     case 'booking':
       return <Booking />
     case 'mybooking':
-      return <Sorry />
+      return <MyBooking />
     case 'inbox':
       return <Inbox />
     case 'contactus':
